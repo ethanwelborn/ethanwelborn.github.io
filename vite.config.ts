4715716a6ba19c1aa2e0cd89c76a2e084a0b7df9
Ethan Welborn <ethan@ethanwelborn.com>
2025-04-04 20:57:14 -0500

    using dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ethanwelborn.github.io/',
+  base: '/ethanwelborn.github.io/dist',
   plugins: [react()],
 })
